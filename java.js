242f5c9a2ca7920966e99fae978137184b9ab2fa
VirsatusCodes <donavynpence@yahoo.com>
2022-05-10 16:02:58 -0700

    Got caught up in the fun and forgot to commit 1. created functions for adding, subtracting, multiplying, and dividing. as well as a function that runs them when called. 2. wired the numbered buttons to values. 3. applied the current value to the display. 4.wired the operations to their buttons. and get 'a' value from pressing and 'func'. 5.clear now wired and resets values. 6. enter gets 'b' value and calls the operate function and displays the value. currently trying to integrate chaining operations, then need to make it so you cant spam enter and continue the last operation called or should i? at least i will make it so it doesnt use the original values and will make it scale with continuous presses, one of the two will be done.

---
+++ java.js
@@ -0,0 +1,122 @@
+function add(a, b){
+    return a + b;
+};
+
+function subtract(a, b){
+    return a - b;
+};
+
+function multiply(a, b){
+    return a * b;
+};
+
+function dividing(a, b){
+    return a/b;
+};
+
+function operate(func,a,b) {
+    if (func === 'add') return add(a,b);
+    if (func === 'subtract') return subtract(a,b);
+    if (func === 'multiply') return multiply(a,b);
+    if (func === 'dividing') return dividing(a,b);
+}
+
+a = 0;
+b = 0;
+func = '';
+const displayValue = document.querySelector('.display');
+
+let displayNumbers = displayValue.textContent;
+
+const zero = document.querySelector('#zero');
+zero.addEventListener('click', () => {
+    displayValue.textContent= displayValue.textContent + '0';
+    displayNumbers = displayValue.textContent;
+});
+const one = document.querySelector('#one');
+one.addEventListener('click', () => {
+    displayValue.textContent= displayValue.textContent + '1';
+    displayNumbers = displayValue.textContent;
+});
+const two = document.querySelector('#two');
+two.addEventListener('click', () => {
+    displayValue.textContent= displayValue.textContent + '2';
+    displayNumbers = displayValue.textContent;
+});
+const three = document.querySelector('#three');
+three.addEventListener('click', () => {
+    displayValue.textContent= displayValue.textContent + '3';
+    displayNumbers = displayValue.textContent;
+});
+const four = document.querySelector('#four');
+four.addEventListener('click', () => {
+    displayValue.textContent= displayValue.textContent + '4';
+    displayNumbers = displayValue.textContent;
+});
+const five = document.querySelector('#five');
+five.addEventListener('click', () => {
+    displayValue.textContent= displayValue.textContent + '5';
+    displayNumbers = displayValue.textContent;
+});
+const six = document.querySelector('#six');
+six.addEventListener('click', () => {
+    displayValue.textContent= displayValue.textContent + '6';
+    displayNumbers = displayValue.textContent;
+});
+const seven = document.querySelector('#seven');
+seven.addEventListener('click', () => {
+    displayValue.textContent= displayValue.textContent + '7';
+    displayNumbers = displayValue.textContent;
+});
+const eight = document.querySelector('#eight');
+eight.addEventListener('click', () => {
+    displayValue.textContent= displayValue.textContent + '8';
+    displayNumbers = displayValue.textContent;
+});
+const nine = document.querySelector('#nine');
+nine.addEventListener('click', () => {
+    displayValue.textContent= displayValue.textContent + '9';
+    displayNumbers = displayValue.textContent;
+});
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    displayValue.textContent= '';
+    displayNumbers = displayValue.textContent;
+    a = 0;
+    b= 0;
+    func = '';
+});
+const enter = document.querySelector('#enter');
+enter.addEventListener('click', () => {
+    b = Number(displayValue.textContent);
+    displayValue.textContent= operate(func,a,b);
+})
+const plus = document.querySelector('#plus');
+plus.addEventListener('click', () => {
+    if (a ===  a > 1) b = Number(displayValue.textContent) =operate(plus,a,b);
+    a = Number(displayValue.textContent);
+    func = 'add';
+    displayValue.textContent = '';
+});
+const minus = document.querySelector('#minus');
+minus.addEventListener('click', () => {
+    a = Number(displayValue.textContent);
+    func = 'subtract';
+    displayValue.textContent = '';
+});
+const times = document.querySelector('#times');
+times.addEventListener('click', () => {
+    a = Number(displayValue.textContent);
+    func = 'multiply';
+    displayValue.textContent = '';
+});
+const divide = document.querySelector('#divide');
+divide.addEventListener('click', () => {
+    a = Number(displayValue.textContent);
+    func = 'dividing';
+    displayValue.textContent = '';
+});
+//dont forget to convert string into numbers if necessary.+
+
+//implement a counter in enter so it cant continue running if no other key 
+//has been pressed, or other logic.
