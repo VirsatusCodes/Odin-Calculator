d6c60669d5fe78cacb32bc0829fb6907fbbc4f22
VirsatusCodes <donavynpence@yahoo.com>
2022-05-11 15:09:38 -0700

    value name changes, function alters. changed a, and b to have more meaningful terminology, also began better tracking values by updating everytime a number is entered. also to stop from having to put Number() everywhere i have added it instead to the operate function at the begining for ease. removed if statements and am planning out a more straightforward logic.

--- java.js
+++ java.js
@@ -15,105 +15,104 @@ function dividing(a, b){
 };
 
 function operate(func,a,b) {
-    if (func === 'add') return add(a,b);
-    if (func === 'subtract') return subtract(a,b);
-    if (func === 'multiply') return multiply(a,b);
-    if (func === 'dividing') return dividing(a,b);
+    if (func === 'add') return add(Number(a),Number(b));
+    if (func === 'subtract') return subtract(Number(a),Number(b));
+    if (func === 'multiply') return multiply(Number(a),Number(b));
+    if (func === 'dividing') return dividing(Number(a),Number(b));
 }
 
-a = 0;
-b = 0;
-func = '';
+let totalValue = 0;
+let func = '';
 const displayValue = document.querySelector('.display');
 
-let displayNumbers = displayValue.textContent;
+let currentValue = 0;
 
 const zero = document.querySelector('#zero');
 zero.addEventListener('click', () => {
-    displayValue.textContent= displayValue.textContent + '0';
-    displayNumbers = displayValue.textContent;
+    displayValue.textContent= displayValue.textContent + 0;
+    currentValue = displayValue.textContent;
 });
 const one = document.querySelector('#one');
 one.addEventListener('click', () => {
-    displayValue.textContent= displayValue.textContent + '1';
-    displayNumbers = displayValue.textContent;
+    displayValue.textContent= displayValue.textContent + 1;
+    currentValue = displayValue.textContent;
 });
 const two = document.querySelector('#two');
 two.addEventListener('click', () => {
-    displayValue.textContent= displayValue.textContent + '2';
-    displayNumbers = displayValue.textContent;
+    displayValue.textContent= displayValue.textContent + 2;
+    currentValue = displayValue.textContent;
 });
 const three = document.querySelector('#three');
 three.addEventListener('click', () => {
-    displayValue.textContent= displayValue.textContent + '3';
-    displayNumbers = displayValue.textContent;
+    displayValue.textContent= displayValue.textContent + 3;
+    currentValue = displayValue.textContent;
 });
 const four = document.querySelector('#four');
 four.addEventListener('click', () => {
-    displayValue.textContent= displayValue.textContent + '4';
-    displayNumbers = displayValue.textContent;
+    displayValue.textContent= displayValue.textContent + 4;
+    currentValue = displayValue.textContent;
 });
 const five = document.querySelector('#five');
 five.addEventListener('click', () => {
-    displayValue.textContent= displayValue.textContent + '5';
-    displayNumbers = displayValue.textContent;
+    displayValue.textContent= displayValue.textContent + 5;
+    currentValue = displayValue.textContent;
 });
 const six = document.querySelector('#six');
 six.addEventListener('click', () => {
-    displayValue.textContent= displayValue.textContent + '6';
-    displayNumbers = displayValue.textContent;
+    displayValue.textContent= displayValue.textContent + 6;
+    currentValue = displayValue.textContent;
 });
 const seven = document.querySelector('#seven');
 seven.addEventListener('click', () => {
-    displayValue.textContent= displayValue.textContent + '7';
-    displayNumbers = displayValue.textContent;
+    displayValue.textContent= displayValue.textContent + 7;
+    currentValue = displayValue.textContent;
 });
 const eight = document.querySelector('#eight');
 eight.addEventListener('click', () => {
-    displayValue.textContent= displayValue.textContent + '8';
-    displayNumbers = displayValue.textContent;
+    displayValue.textContent= displayValue.textContent + 8;
+    currentValue = displayValue.textContent;
 });
 const nine = document.querySelector('#nine');
 nine.addEventListener('click', () => {
-    displayValue.textContent= displayValue.textContent + '9';
-    displayNumbers = displayValue.textContent;
+    displayValue.textContent= displayValue.textContent + 9;
+    currentValue = displayValue.textContent;
 });
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
     displayValue.textContent= '';
-    displayNumbers = displayValue.textContent;
-    a = 0;
-    b= 0;
+    currentValue = displayValue.textContent;
+    currentValue = 0;
+    totalValue = 0;
     func = '';
 });
 const enter = document.querySelector('#enter');
 enter.addEventListener('click', () => {
-    b = Number(displayValue.textContent);
-    displayValue.textContent= operate(func,a,b);
+    currentValue = (displayValue.textContent);  
+    totalValue = (operate(func,currentValue,totalValue));
+    displayValue.textContent = totalValue;
 })
 const plus = document.querySelector('#plus');
 plus.addEventListener('click', () => {
-    if (a > 1) b = Number(displayValue.textContent);
-    if (b > 1) displayValue.textContent = operate('add',a,b);
-    if (a === 0) a = Number(displayValue.textContent);
+    totalValue =displayValue.textContent;
+    currentValue = 0
     func = 'add';
-    if (b ===0 ) displayValue.textContent = '';
+    displayValue.textContent = '';
 });
 const minus = document.querySelector('#minus');
 minus.addEventListener('click', () => {
-    a = Number(displayValue.textContent);
+    currentValue =displayValue.textContent;
     func = 'subtract';
     displayValue.textContent = '';
 });
 const times = document.querySelector('#times');
 times.addEventListener('click', () => {
-    a = Number(displayValue.textContent);
+    currentValue = displayValue.textContent;
     func = 'multiply';
     displayValue.textContent = '';
 });
 const divide = document.querySelector('#divide');
 divide.addEventListener('click', () => {
-    a = Number(displayValue.textContent);
+    currentValue = displayValue.textContent;
     func = 'dividing';
     displayValue.textContent = '';
 });
